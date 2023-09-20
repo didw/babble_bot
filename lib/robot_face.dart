adedd07de174cfd4341a84a827d982b3fac10f4a
Jongyeol Yang <yangjy0113@gmail.com>
2023-09-21 08:05:55 +0900

    [FIX] initialization error

--- lib/robot_face.dart
+++ lib/robot_face.dart
@@ -18,21 +18,14 @@ class _RobotFaceState extends State<RobotFace>
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _animation,
-      builder: (context, child) {
-        return Container(
-          // 불빛 깜빡임을 시뮬레이션하기 위해 색상을 변경
-          color: _animation.value > 0.5 ? Colors.red : Colors.blue,
-          width: 100,
-          height: 100,
-          // 여기에 로봇 얼굴 UI를 추가할 수 있습니다.
-          child: Icon(
-            Icons.face, // 로봇 얼굴을 표현하는 아이콘입니다. 원하시는 이미지로 교체 가능합니다.
-            size: 200,
-          ),
-        );
-      },
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.face,
+          size: 200,
+        ),
+      ],
     );
   }
 
